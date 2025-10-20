5cc211b05045b1c93b91a8ae1a578eb9271871eb
ineza kevin <k.ineza1@alustudent.com>
2025-10-20 12:46:37 +0000

    gathered data from an api

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+Script that uses a REST API to get TODO list progress for a given employee ID.
+"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    # Check that the user provided an ID as an argument
+    if len(sys.argv) != 2:
+        print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = sys.argv[1]
+
+    # Define URLs
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+
+    # Get user data
+    user_response = requests.get(user_url)
+    todo_response = requests.get(todos_url)
+
+    # Check content type before calling .json()
+    if "application/json" in user_response.headers.get("Content-Type", ""):
+        user_data = user_response.json()
+    else:
+        print("User data is not in JSON format.")
+        sys.exit(1)
+
+    if "application/json" in todo_response.headers.get("Content-Type", ""):
+        todos_data = todo_response.json()
+    else:
+        print("TODO data is not in JSON format.")
+        sys.exit(1)
+
+    # Extract employee name
+    employee_name = user_data.get("name")
+
+    # Filter tasks
+    completed_tasks = [task for task in todos_data if task.get("completed")]
+    total_tasks = len(todos_data)
+    done_tasks = len(completed_tasks)
+
+    # Display progress
+    print(f"Employee {employee_name} is done with tasks({done_tasks}/{total_tasks}):")
+
+    # Print titles of completed tasks
+    for task in completed_tasks:
+        print(f"\t {task.get('title')}")
